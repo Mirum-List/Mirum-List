be1b97e7124fdc44de836b5ae863700bdbc25a49
sumi <lsmkoo03@naver.com>
2024-11-25 23:41:01 +0900

    리스트 보기, 캘린더, 리스트 편집, 로그 화면 생성

---
+++ lib/editList/edit_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EditListScreen extends StatelessWidget {
+  const EditListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text("리스트 편집 보기"));
+  }
+}
