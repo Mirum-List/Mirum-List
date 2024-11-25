1b8d5ff3d7b523b94a43c3e3bce4c345eabdcd83
sumi <lsmkoo03@naver.com>
2024-11-25 23:38:09 +0900

    색상 상수 파일 추가

---
+++ lib/const/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const whiteColor = Color(0xFFFFFFFF);
+const mainColor = Color(0xFF90AFDD);
+const moreDeepBlueColor = Color(0xFF3564AA);
+const deepBlueColor = Color(0xFF5B87C8);
+const normalBlueColor = Color(0xFF90AFDD);
+const lightBlueColor = Color(0xFFADCDFC);
+const normalRedColor = Color(0xFFE57373);
